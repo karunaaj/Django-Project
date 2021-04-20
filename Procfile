1a95d7eaac4c262c854f15a79b283beb622f127d
karuna <karuna@abc.com>
2021-04-20 20:19:26 +0530

    Updated static

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn airoguard.wsgi --log-file -
--- airoguard/settings.py
+++ airoguard/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
